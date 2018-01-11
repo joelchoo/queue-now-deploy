3ea354225e77680b63993faa7cf00173b7a97f98
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-01-11 18:15:00 +0800

    Updates

--- push-listener.js
+++ push-listener.js
@@ -1,12 +1,36 @@
+
+function openOrFocusPage (targetURL) {
+  const showPage = self.clients
+    .matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    })
+    .then((windowClients) => {
+      const urlToOpen = new URL(targetURL, self.location.origin).href
+      const matchingClient = windowClients.find(windowClient => windowClient.url === urlToOpen)
+      return matchingClient ? matchingClient.focus() : self.clients.openWindow(urlToOpen)
+    })
+  return showPage
+}
+
 self.addEventListener('push', function (event) {
   if (event.data) {
-    const payload = event.data.json()
-    const position = Number(payload.position)
+    const data = event.data.json()
+    const position = Number(data.position)
     const body = position > 0
       ? `You're ${position} places away from being served.`
       : "It's now your turn! Please be ready to be served."
-    const notifyUser = self.registration.showNotification(payload.queueName, { body })
-    event.waitUntil(notifyUser)
+    event.waitUntil(self.registration.showNotification(data.queueName, { body, data }))
+  } else {
+    console.log('This push event has no data.')
+  }
+})
+
+self.addEventListener('notificationclick', function (event) {
+  if (event.notification.data) {
+    const {data} = event.notification
+    const targetURL = `./#/queue/${data.keys.queue}/status/${data.keys.nowServing}`
+    event.waitUntil(openOrFocusPage(targetURL))
   } else {
     console.log('This push event has no data.')
   }
